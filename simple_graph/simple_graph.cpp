c449d4f4f0791195f8f2b7c39c34a582fbf912db
graph-walker <68038077+graph-walker@users.noreply.github.com>
2020-07-18 18:12:15 -0700

    Add files via upload

---
+++ simple_graph/simple_graph.cpp
@@ -0,0 +1,54 @@
+#include <vector>
+#include <map>
+#include <iostream>
+
+// simple, undirected, unweighted, graph
+
+struct Graph
+{
+	int num_vertices_{ 0 };
+	std::map<int, std::vector<int>> vertex_connections_;
+	std::vector<std::vector<int>>  adjacency_matrix_;
+	Graph(int num_vertices, std::map<int, std::vector<int>> connections) :
+		num_vertices_{ num_vertices }, vertex_connections_{ connections }
+	{
+		for (int i = 0; i< num_vertices; i++)
+		{
+			std::vector<int> row;
+			for (int j = 0; j< num_vertices; j++)
+			{
+				row.emplace_back(0);
+			}
+			adjacency_matrix_.emplace_back(row);
+		}
+
+		for (const auto& pair : connections) {
+			for (auto&& it : pair.second) {
+				adjacency_matrix_[pair.first][it] = 1;
+			}
+		}
+	};
+
+	void visualize()
+	{
+		for (auto&& row : this->adjacency_matrix_) 
+		{
+			for (auto&& it : row)
+			{
+				std::cout << it << " ";
+			}
+			std::cout << "\n";
+		}
+	}
+};
+
+// example graph
+int main()
+{
+	std::map<int, std::vector<int>> verts;
+	verts[2] = std::vector<int>{ 1,0 };
+	verts[0] = std::vector<int>{ 2 };
+
+	Graph g{ 3, verts };
+	g.visualize();
+}
